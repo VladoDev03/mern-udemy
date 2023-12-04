fa700a0bd150fadb48e2547a4bde564657ff39b5
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-05 00:10:18 +0200

    Completed refresh-react section.

--- refresher-react/src/App.js
+++ refresher-react/src/App.js
@@ -1,11 +1,24 @@
+import { useState } from 'react';
 import './App.css';
-import GoalList from './components/GoalList';
+import GoalList from './components/goal-list/GoalList';
+import NewGoal from './components/new-goal/NewGoal';
 
 function App() {
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the Course' },
+    { id: 'cg2', text: 'Learn all about the Course Main Topic' },
+    { id: 'cg3', text: 'Help other students in the Course Q&A' }
+  ]);
+
+  const addNewHandler = (newGoal) => {
+    setCourseGoals(oldState => oldState.concat(newGoal));
+  };
+
   return (
     <div className='course-goals'>
       <h2>Course Goals</h2>
-      <GoalList />
+      <NewGoal onAddGoal={addNewHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
---
+++ refresher-react/src/components/goal-list/GoalList.js
@@ -0,0 +1,11 @@
+import './GoalList.css';
+
+const GoalList = props => {
+    return (
+        <ul className='goal-list'>
+            {props.goals.map(goal => <li key={goal.id}>{goal.text}</li>)}
+        </ul>
+    );
+};
+
+export default GoalList;
