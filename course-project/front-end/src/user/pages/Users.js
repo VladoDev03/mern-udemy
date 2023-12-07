bd39675035a73bb79d3fb3db589d3f7ea11d9024
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-07 19:24:30 +0200

    Added users-list and user-item components.

--- course-project/front-end/src/user/pages/Users.js
+++ course-project/front-end/src/user/pages/Users.js
@@ -1,5 +1,18 @@
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-    return <h2>Users Works!</h2>
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Loki',
+            image: 'https://www.slashfilm.com/img/gallery/marvels-loki-season-2-ending-explained-is-this-truly-glorious-purpose/the-god-of-stories-1699636943.jpg',
+            places: 3
+        }
+    ];
+
+    return (
+        <UsersList items={USERS} />
+    );
 };
 
 export default Users;
