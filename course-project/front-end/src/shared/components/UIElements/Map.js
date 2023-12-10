b79d5a87bea7c4878cab4d6811cf824e962c8881
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-10 19:51:56 +0200

    Added OpenLayers for map displaying.

---
+++ course-project/front-end/src/shared/components/UIElements/Map.js
@@ -0,0 +1,42 @@
+import { useRef, useEffect } from "react";
+
+import "./Map.css";
+
+const Map = (props) => {
+    const mapRef = useRef();
+    const { center, zoom } = props;
+
+    useEffect(() => {
+        // Google maps
+        // const map = new window.google.maps.Map(mapRef.current, {
+        //     center: center,
+        //     zoom: zoom,
+        // });
+
+        // new window.google.maps.Marker({ position: center, map: map });
+
+        new window.ol.Map({
+            target: mapRef.current.id,
+            layers: [
+                new window.ol.layer.Tile({
+                    source: new window.ol.source.OSM()
+                })
+            ],
+            view: new window.ol.View({
+                center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+                zoom: zoom
+            })
+        });
+    }, [center, zoom]);
+
+    return (
+        <div
+            ref={mapRef}
+            className={`map ${props.className}`}
+            style={props.style}
+            id='map'
+        ></div>
+    );
+};
+
+export default Map;
