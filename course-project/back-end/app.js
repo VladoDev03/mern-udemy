9cba9a59e768aa0e34ff81ed6ec6cac8277da531
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-23 22:39:39 +0200

    Added get place by id and by userId and added error handling.

--- course-project/back-end/app.js
+++ course-project/back-end/app.js
@@ -5,7 +5,16 @@ const usersRoutes = require('./routes/users-routes');
 
 const app = express();
 
-app.use(placesRoutes);
-app.use(usersRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'An unknown error occured!' });
+});
 
 app.listen(5000);
