38007e0ad740ec1b03938157edbf290bb6265733
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-27 02:24:03 +0200

    Added signup request and CORS policy.

--- course-project/back-end/app.js
+++ course-project/back-end/app.js
@@ -10,6 +10,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    next();
+});
+
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
