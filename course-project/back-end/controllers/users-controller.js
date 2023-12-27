67a649a26c968f48c65e7ec7d92473776f3e31bf
Vladimir Stoyanov <vladsto101@gmail.com>
2023-12-28 00:17:17 +0200

    Added jwt on signup and on login.

--- course-project/back-end/controllers/users-controller.js
+++ course-project/back-end/controllers/users-controller.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -65,7 +66,23 @@ const signup = async (req, res, next) => {
         return next(error);
     }
 
-    res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+    let token;
+
+    try {
+        token = jwt.sign(
+            {
+                userId: createdUser.id,
+                email: createdUser.email
+            },
+            'thisisasicretthatmustnotbeshared',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError('Signing up failed, please try again.', 500);
+        return next(error);
+    }
+
+    res.status(201).json({ userId: createdUser, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -99,7 +116,21 @@ const login = async (req, res, next) => {
         return next(error);
     }
 
-    res.json({ message: 'Logged in!', user: existingUser.toObject({ getters: true }) });
+    try {
+        token = jwt.sign(
+            {
+                userId: existingUser.id,
+                email: existingUser.email
+            },
+            'thisisasicretthatmustnotbeshared',
+            { expiresIn: '1h' }
+        );
+    } catch (err) {
+        const error = new HttpError('Logging in failed, please try again.', 500);
+        return next(error);
+    }
+
+    res.status(201).json({ userId: existingUser, email: existingUser.email, token: token });
 };
 
 exports.getUsers = getUsers;
